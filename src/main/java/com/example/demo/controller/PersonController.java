22980d30edf5372caf6551b60b241ed06ac137fb
ReemyHasan <reemhasan2000831@gmail.com>
2022-11-07 22:15:20 +0200

    Demo1

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -12,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping (value = "/Persons")
+@RequestMapping (value = "/persons")
 public class PersonController {
 
     @Autowired
